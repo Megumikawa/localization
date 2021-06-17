890baa77139a1fdef78fc5fc3b431e490bc96d35
Megumikawa <megumi19901016@gmail.com>
2021-06-17 15:55:40 +0200

    created base until buton with flag

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import { useTranslation } from "react-i18next";
+
+
+const languages = [
+  {
+    code: 'fr',
+    name: 'Français',
+    country_code: 'fr'
+  },
+  {
+    code: 'en',
+    name: 'English',
+    country_code: 'gb'
+  },
+  {
+    code: 'ar',
+    name: 'العربية',
+    country_code: 'sa'
+  },
+]
+
 
 function App() {
+  const { t } = useTranslation()
+
+  const releaseDate = new Date('2021-06-17')
+  const timeDifference = new Date() - releaseDate
+  const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+
   return (
-    <div className="App">
-      Hello World
+    <div className="container">
+      <div className="d-flex justify-content-end">
+      <div className="dropdown">
+        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+        Dropdown button
+        </button>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+          {languages.map(({ code, name, country_code}) => (
+          <li key={country_code}>
+            <button className="dropdown-item">
+            <span className={`flag-icon flag-icon-${country_code} mx-2`}></span>
+              {name}
+            </button>
+          </li>
+        ))}
+        </ul>
+      </div>
+      </div>
+      <div className="d-flex flex-column align-items-start">
+        <h1 className="font-weight-normal mb-3">{t('welcome_message')}</h1>
+        <p>{t('days_since_release', { number_of_days })}</p>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,39 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import i18n from "i18next";
-import { useTranslation, initReactI18next } from "react-i18next";
-
-import './index.css';
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'flag-icon-css/css/flag-icon.min.css';
 import App from './App';
 
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi )
   .init({
-    // the translations
-    // (tip move them in a JSON file and import them,
-    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
-    resources: {
-      en: {
-        translation: {
-          "Welcome to React": "Welcome to React and react-i18next"
-        }
-      }
-    },
-    lng: "en", // if you're using a language detector, do not define the lng option
+    supportedLngs: ['en', 'fr', 'ar'],
     fallbackLng: "en",
-
-    interpolation: {
-      escapeValue: false
-    }
+    detection: {
+      order: ['cookie', 'htmlTag', 'localStorage', 'path', 'subdomain'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react: { useSuspense: false },
   });
-
-function App() {
-  const { t } = useTranslation();
-
-  return <h2>{t('Welcome to React')}</h2>;
-}
 
 
 ReactDOM.render(
